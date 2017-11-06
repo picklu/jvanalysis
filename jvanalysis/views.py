7aee795b986ff4a8afe1ba125c718155b5536ec3
Subrata Sarker <subrata_sarker@yahoo.com>
2017-11-06 03:15:12 +0000

    Create all the basic templates except signup

--- jvanalysis/views.py
+++ jvanalysis/views.py
@@ -4,10 +4,29 @@ from flask import url_for
 from jvanalysis import app
 
 @app.route("/")
+def index():
+    return render_template("home.html")
+
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home_secured.html")
 
 @app.route("/about")
 def about():
-    return render_template("home_secured.html")
+    return render_template("about.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/signout")
+def signout():
+    return index()
