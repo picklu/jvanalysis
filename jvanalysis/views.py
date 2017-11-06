a36e7526211221773c93081636dfe157c8b6a0c6
Subrata Sarker <subrata_sarker@yahoo.com>
2017-11-06 07:11:09 +0000

    Dynamically update active class in the menu using

--- jvanalysis/views.py
+++ jvanalysis/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from flask import url_for
 
 from jvanalysis import app
@@ -13,24 +13,36 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 @app.route("/account")
 def account():
-    return render_template("account.html")
+    return render_template("account.html", title="Account")
 
 @app.route("/analysis")
 def analysis():
-    return render_template("analysis.html")
+    return render_template("analysis.html", title="Analysis")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", title="Dashboard")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html", title="Sign up")
+    return index()
 
-@app.route("/signout")
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "GET":
+        return render_template("signin.html", title="Sign in")
+    return index()
+
+@app.route("/signout", methods=["POST"])
 def signout():
     return index()
+
+@app.route("/guest")
+def guest():
+    return home()
