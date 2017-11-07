5781148aa0807696912b2a50d2251f264a994268
Subrata Sarker <subrata_sarker@yahoo.com>
2017-11-07 06:02:39 +0000

    Draw x=0 and y=0 grid

--- jvanalysis/jvplot.py
+++ jvanalysis/jvplot.py
@@ -2,7 +2,7 @@ import numpy as np
 from jvanalysis.analysis import Analysis, AnalysisError
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import Range1d
+from bokeh.models import Range1d, Span
 from bokeh.resources import INLINE
 
 
@@ -20,11 +20,19 @@ def resources(width=400, height=400):
                     plot_width=width, 
                     plot_height=height
                 )
-    plot.line(data1[0], data1[1] * 1000 / 0.25, legend="D1_OCtoSC_SR500mVps", line_color="blue", line_width=2)
-    plot.line(data2[0], data2[1] * 1000 / 0.25, legend="D1_SCtoOC_SR500mVps", line_color="red", line_width=2)    
+    
+    # Vertical line
+    vline = Span(location=0, dimension='height', line_color='gray', line_width=1)
+    # Horizontal line
+    hline = Span(location=0, dimension='width', line_color='gray', line_width=1)
+    plot.renderers.extend([vline, hline])
+    
+    # line plot
+    plot.line(data1[0], data1[1] * 1000 / 0.25, legend="OC_SC", line_color="blue", line_width=2)
+    plot.line(data2[0], data2[1] * 1000 / 0.25, legend="SC_OC", line_color="red", line_width=2)    
     plot.x_range = Range1d(-0.6, 0.1)
     plot.y_range = Range1d(-5, 20)
-    plot.legend.location = "bottom_right"
+    plot.legend.location = "top_left"
     plot.xaxis.axis_label = "Voltage (V)"
     plot.yaxis.axis_label = r"Current Density (mA/cm^2)"
     
