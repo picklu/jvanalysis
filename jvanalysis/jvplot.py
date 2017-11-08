f3ed968fa708bdb2c1a7a4361040e217042b2ca3
Subrata Sarker <subrata_sarker@yahoo.com>
2017-11-08 06:28:44 +0000

    Update plot title

--- jvanalysis/jvplot.py
+++ jvanalysis/jvplot.py
@@ -9,7 +9,7 @@ from bokeh.resources import INLINE
 DATA_FOLDER = "/home/ubuntu/workspace/jvanalysis/data"
 TOOLS = "wheel_zoom,box_zoom,reset,save"
 
-def resources(width=140, height=100):
+def resources(width=130, height=100):
     """ return bokeh resources for plot
     """
     data = np.transpose(np.loadtxt("/home/ubuntu/workspace/jvanalysis/data/D1_SCtoOC_SR500mVps.txt"))
@@ -24,13 +24,8 @@ def resources(width=140, height=100):
         label = e
         print(e)
     
-    plot = figure(
-                    title="Current vs Voltage Curves of DSSCS", 
-                    tools=TOOLS,
-                    plot_width=width, 
-                    plot_height=height,
-                    sizing_mode='scale_width'
-                )
+    plot = figure(title="jV Plot", tools=TOOLS, plot_width=width, 
+                plot_height=height,sizing_mode='scale_width')
     
     # Vertical line
     vline = Span(location=0, dimension='height', line_color='gray', line_width=1)
@@ -40,11 +35,11 @@ def resources(width=140, height=100):
     
     if label == "calculated":
         # line plot
-        plot.circle(jv.v_cell, jv.j_cell*1000, legend="experimental", line_color="blue", line_width=2)
+        plot.circle(jv.v_cell, jv.j_cell*1000, legend="experimental", line_color="green", fill_color=None, line_width=2)
         plot.line(jv.v_cell, j*1000, legend="calculated", line_color="red", line_width=2)    
     else:
         # line plot
-        plot.circle(data[0], data[1]*1000/0.25, legend="experimental", line_color="blue", line_width=2)
+        plot.circle(data[0], data[1]*1000/0.25, legend="experimental", line_color="green", fill_color=None, line_width=2)
         plot.line(data[0], data[1]*1000/0.25, legend="failed", line_color="blue", line_width=2)
     plot.x_range = Range1d(-0.6, 0.1)
     plot.y_range = Range1d(-5, 20)
