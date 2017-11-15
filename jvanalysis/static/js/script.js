32853933c3b4f512c2af5b5ddb5acf53bca72761
Subrata Sarker <subrata_sarker@yahoo.com>
2017-11-15 08:02:30 +0000

    Add toggleEdit() for data table to edit the data

--- jvanalysis/static/js/script.js
+++ jvanalysis/static/js/script.js
@@ -19,12 +19,13 @@ function showData(data) {
         $dt.html(null);
         $(data).each((i, row) => {
             var $rowTable = $('<tr/>');
-            $rowTable.html(`<th><input id="dt${i}" type="checkbox"> ${i+1}</th>`);
+            $rowTable.html(`<td><input id="data-${i}" type="button" class="btn btn-sm my-sm-0 btn-primary editable" value="Edit"></td>`);
             $(row).each((j, col) => {
-                $rowTable.append(`<td>${col}</td>`);
+                $rowTable.append(`<td><span class="data-${i}">${col}</span></td>`);
             });
             $dt.append($rowTable);
         });
+        toggleEdit();
     }
     else {
         $dt.html('No data found!');
@@ -37,4 +38,26 @@ function saveLocal(file, data) {
         fSize: file.size,
         data: data
     }));
+}
+
+function toggleEdit() {
+    $('td input[type="button"]').on("click", (event) => {
+        var $this = $(event.currentTarget);
+        var $dataSpan = $(`.${$this.prop('id')}`);
+        
+        if ($this.val() == 'Edit') {
+            $this.val('Done')
+                 .addClass('btn-info')
+                 .removeClass('btn-primary');
+            $dataSpan.attr('contenteditable', true)
+                     .addClass('lead');
+        }
+        else {
+            $this.val('Edit')
+                 .addClass('btn-primary')
+                 .removeClass('btn-info');
+            $dataSpan.attr('contenteditable', false)
+                     .removeClass('lead');
+        }
+    });
 }
