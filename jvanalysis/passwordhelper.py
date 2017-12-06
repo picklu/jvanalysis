3833ddcfeffaae92bd0fa8ba56c15091727280e2
Subrata Sarker <subrata_sarker@yahoo.com>
2017-12-06 02:44:58 +0000

    Update passwordhelper.py with comments

--- jvanalysis/passwordhelper.py
+++ jvanalysis/passwordhelper.py
@@ -1,18 +1,19 @@
 # *************************************************
+# Modified from
 # Book: Flask: Building Python Web Services
 # By Gareth Dwyer, Shalabh Aggarwal, Jack Stouffer
 # *************************************************
-import base64
-import hashlib
 import os
+from base64 import b64encode
+from hashlib import sha512
 
 class PasswordHelper(object):
 
     def get_hash(self, plain):
-        return hashlib.sha512(plain.encode('utf-8')).hexdigest()
+        return sha512(plain.encode('utf-8')).hexdigest()
 
     def get_salt(self):
-        return base64.b64encode(os.urandom(20)).decode()
+        return b64encode(os.urandom(20)).decode()
 
     def validate_password(self, plain, salt, expected):
         return self.get_hash(plain + salt) == expected
