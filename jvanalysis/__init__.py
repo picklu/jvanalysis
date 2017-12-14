9b8bfee584e334b01417e2ee5e7654365f30ff4f
Subrata Sarker <subrata_sarker@yahoo.com>
2017-12-14 12:50:13 +0000

    Use MongoClient from flask_pymongo

--- jvanalysis/__init__.py
+++ jvanalysis/__init__.py
@@ -3,18 +3,17 @@ from flask import Flask
 from flask_jsglue import JSGlue
 from flask_login import LoginManager
 from flask_session import Session
+from flask_pymongo import MongoClient
 
 from jvanalysis.jvhelper import nice_date
 from jvanalysis.jvhelper import nice_number
 from jvanalysis.jvhelper import ObjectIDConverter
 from jvanalysis.jvhelper import nicefy
 
-from pymongo import MongoClient
 
 app = Flask(__name__)
 app.url_map.converters['objectid'] = ObjectIDConverter
 app.config.from_object('config.DevelopmentConfig')
-app.config['SESSION_MONGODB'] = MongoClient()
 
 app.jinja_env.filters['nicefy'] = nicefy
 app.jinja_env.filters['nicedate'] = nice_date
